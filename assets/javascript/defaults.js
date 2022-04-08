4b7658df8154d97d89f019d2aa35932184b1ce88
Hg <83497023+HgVN23@users.noreply.github.com>
2022-04-08 15:59:02 +0700

    Add ads

--- assets/javascript/defaults.js
+++ assets/javascript/defaults.js
@@ -3,7 +3,7 @@ function defaults() {
 	// Script
 	for (let script of scriptList) {
 		let scriptCreate = document.createElement('script');
-		scriptCreate.src = `https://hgvn23.github.io/assets/javascript/${script}.js`;
+		scriptCreate.src = `assets/javascript/${script}.js`;
 		document.getElementById("script").appendChild(scriptCreate);
 	};
 	// ======================================================================
@@ -23,7 +23,8 @@ let scriptList = [
 	"filter",
 	"search",
 	"dropdown",
-	"theme"
+	"theme",
+	"ads"
 ];
 let header = `
 	<div class="sidebarIcon sidebarIconClose" onclick="sidebarOpen()"></div>
@@ -173,13 +174,5 @@ let footer = `
 		<div class="copyright3">Hosted by <a class="textLink" href="https://github.com/" target="_blank">Github</a></div>
 	</div>
 	<!-- Ads 1 -->
-	<ins class="adsbygoogle"
-	     style="display:block"
-	     data-ad-client="ca-pub-2430509461017367"
-	     data-ad-slot="6618344666"
-	     data-ad-format="auto"
-	     data-full-width-responsive="true"></ins>
-	<script>
-	     (adsbygoogle = window.adsbygoogle || []).push({});
-	</script>
+	<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-2430509461017367" data-ad-slot="6618344666" data-ad-format="auto" data-full-width-responsive="true"></ins>
 `;
