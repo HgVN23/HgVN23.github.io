f51037efc610b0b2f3689ba4c590974e6f10d65f
Hg <83497023+HgVN23@users.noreply.github.com>
2022-04-13 21:35:38 +0700

    Ads go brrr

--- assets/javascript/ads.js
+++ assets/javascript/ads.js
@@ -1,2 +1,35 @@
+let adsPc = `
+	<div class="contentGroup">
+		<div class="contentIcon iconLuuY"></div>
+		<div class="contentTitle">Ads</div>
+	</div>
+	<!-- Ads 1 -->
+	<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-2430509461017367" data-ad-slot="6618344666" data-ad-format="auto" data-full-width-responsive="true"></ins>
+	<!-- Ads 2 -->
+	<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-2430509461017367" data-ad-slot="1982672065" data-ad-format="auto" data-full-width-responsive="true"></ins>
+	<!-- Ads 3 -->
+	<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-2430509461017367" data-ad-slot="7686386895" data-ad-format="auto" data-full-width-responsive="true"></ins>
+`;
+let adsPhone = `
+	<div class="contentGroup">
+		<div class="contentIcon iconLuuY"></div>
+		<div class="contentTitle">Ads</div>
+	</div>
+	<!-- Ads 4 -->
+	<ins class="adsbygoogle" style="display:inline-block;width:300px;height:300px" data-ad-client="ca-pub-2430509461017367" data-ad-slot="1239821054"></ins>
+`;
+
+// AdsPc
+let adsCreatePc = document.createElement('div');
+adsCreatePc.classList.add('contentBox', 'adsPc');
+adsCreatePc.innerHTML = adsPc;
+document.querySelector(".content").appendChild(adsCreatePc);
+
+// AdsPhone
+let adsCreatePhone = document.createElement('div');
+adsCreatePhone.classList.add('contentBox', 'adsPhone');
+adsCreatePhone.innerHTML = adsPhone;
+document.querySelector(".content").appendChild(adsCreatePhone);
+
 for (var i = 0; i < 3; i++)
 	(adsbygoogle = window.adsbygoogle || []).push({});
--- assets/javascript/defaults.js
+++ assets/javascript/defaults.js
@@ -17,12 +17,6 @@ function defaults() {
 	document.getElementById("footer").innerHTML = footer;
 	document.querySelector(".year").innerHTML = new Date().getFullYear();
 	// ======================================================================
-	// Ads
-	let adsCreate = document.createElement('div');
-	adsCreate.classList.add('contentBox');
-	adsCreate.innerHTML = ads;
-	document.querySelector(".content").appendChild(adsCreate);
-	// ======================================================================
 }
 let scriptList = [
 	"sidebar",
@@ -182,16 +176,4 @@ let footer = `
 			<div class="copyright3">Hosted by <a class="textLink" href="https://github.com/" target="_blank">Github</a></div>
 		</div>
 	</div>
-`;
-let ads = `
-	<div class="contentGroup">
-		<div class="contentIcon iconLuuY"></div>
-		<div class="contentTitle">Ads</div>
-	</div>
-	<!-- Ads 1 -->
-	<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-2430509461017367" data-ad-slot="6618344666" data-ad-format="auto" data-full-width-responsive="true"></ins>
-	<!-- Ads 2 -->
-	<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-2430509461017367" data-ad-slot="1982672065" data-ad-format="auto" data-full-width-responsive="true"></ins>
-	<!-- Ads 3 -->
-	<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-2430509461017367" data-ad-slot="7686386895" data-ad-format="auto" data-full-width-responsive="true"></ins>
 `;
