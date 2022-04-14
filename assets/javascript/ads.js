557133a64b26419ec51f862a72f57df04c45ae42
Hg <83497023+HgVN23@users.noreply.github.com>
2022-04-14 12:06:14 +0700

    Add files via upload

--- assets/javascript/ads.js
+++ assets/javascript/ads.js
@@ -4,16 +4,16 @@ let ads = `
 		<div class="contentTitle">Ads</div>
 	</div>
 	<!-- Ads 1 -->
-	<ins class="adsbygoogle ads" data-ad-client="ca-pub-2430509461017367" data-ad-slot="6618344666" data-ad-format="auto" data-full-width-responsive="true"></ins>
+	<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-2430509461017367" data-ad-slot="6618344666" data-ad-format="auto" data-full-width-responsive="true"></ins>
 	<!-- Ads 2 -->
-	<ins class="adsbygoogle ads" data-ad-client="ca-pub-2430509461017367" data-ad-slot="1982672065" data-ad-format="auto" data-full-width-responsive="true"></ins>
+	<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-2430509461017367" data-ad-slot="1982672065" data-ad-format="auto" data-full-width-responsive="true"></ins>
 	<!-- Ads 3 -->
-	<ins class="adsbygoogle ads" data-ad-client="ca-pub-2430509461017367" data-ad-slot="7686386895" data-ad-format="auto" data-full-width-responsive="true"></ins>
+	<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-2430509461017367" data-ad-slot="7686386895" data-ad-format="auto" data-full-width-responsive="true"></ins>
 `;
 
 // Ads
 let adsCreate = document.createElement('div');
-adsCreate.classList.add('contentBox');
+adsCreate.classList.add('contentBox', 'ads');
 adsCreate.innerHTML = ads;
 document.querySelector(".content").appendChild(adsCreate);
 
