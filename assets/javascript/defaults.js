4c7213084848d5d551f7827090dc242b373634cb
Hg <83497023+HgVN23@users.noreply.github.com>
2022-04-14 11:10:50 +0700

    Update defaults.js

--- assets/javascript/defaults.js
+++ assets/javascript/defaults.js
@@ -3,7 +3,7 @@ function defaults() {
 	// Script
 	for (let script of scriptList) {
 		let scriptCreate = document.createElement('script');
-		scriptCreate.src = `assets/javascript/${script}.js`;
+		scriptCreate.src = `https://hgvn23.github.io/assets/javascript/${script}.js`;
 		document.getElementById("script").appendChild(scriptCreate);
 	};
 	// ======================================================================
