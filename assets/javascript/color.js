681c5271bec98b458e3df347613c056d156598d4
Hg <83497023+HgVN23@users.noreply.github.com>
2022-04-15 11:53:48 +0700

    Add files via upload

---
+++ assets/javascript/color.js
@@ -0,0 +1,5 @@
+var color = document.getElementById("color");
+
+color.addEventListener("input", function(){
+	document.querySelector(".result").innerHTML = parseInt(color.value.slice(1), 16);
+}, false);
