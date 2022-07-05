9711a23a72d0f4c6268e9406ed313f25d4da0baf
Hg <83497023+HgVN23@users.noreply.github.com>
2022-07-05 19:50:25 +0700

    Fix bug glyph on phone

--- assets/javascript/webTool/glyph.js
+++ assets/javascript/webTool/glyph.js
@@ -3,6 +3,9 @@ for(var i = 0; i < option.length; i++) {
 	option[i].addEventListener('click', glyphCreate);
 };
 
+const create = document.getElementById("create");
+create.addEventListener('click', glyphCreate);
+
 const result = document.querySelector(".result");
 
 const glyphTd = document.getElementsByTagName("td");
@@ -20,13 +23,26 @@ function glyphThis() {
 function glyphCreate() {
 	const glyphTable = document.getElementById("glyphTable").value;
 
-	result.innerHTML = `Chọn kí tự`
-	let i = 0;
-	for(let glyphRow of glyphList) {
-		for(let glyphCol of glyphList) {
-			glyphTd[i].innerHTML = `&#x${glyphTable}${glyphRow}${glyphCol};`;
-			i++;
+	if(glyphTable == "")
+		missing();
+	else {
+		result.innerHTML = `Chọn kí tự`
+		let i = 0;
+		for(let glyphRow of glyphList) {
+			for(let glyphCol of glyphList) {
+				glyphTd[i].innerHTML = `&#x${glyphTable}${glyphRow}${glyphCol};`;
+				i++;
+			}
 		}
+	}
+};
+
+function missing() {
+	document.getElementById("create").innerHTML = "Chưa chọn";
+
+	const timeout = setTimeout(reset, 1000);
+	function reset() {
+		document.getElementById("create").innerHTML = "Tạo";
 	}
 };
 
