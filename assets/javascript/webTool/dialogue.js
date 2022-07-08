af1118f773219e845c098cb58e2e9de95454c9db
Hg <83497023+HgVN23@users.noreply.github.com>
2022-07-08 15:50:45 +0700

    Update dialogueGen

--- assets/javascript/webTool/dialogue.js
+++ assets/javascript/webTool/dialogue.js
@@ -1,47 +1,71 @@
+const radio = document.getElementsByClassName("cRadio");
+
+for (var i = 0; i < radio.length; i += 2) {
+	radio[i].addEventListener("click", radioText);
+};
+for (var i = 1; i < radio.length; i += 2) {
+	radio[i].addEventListener("click", radioTranslate);
+};
+function radioText() {
+	this.parentElement.querySelector(".with").style.display = `none`;
+};
+function radioTranslate() {
+	this.parentElement.querySelector(".with").style.display = `block`;
+};
+
 const saveFile = document.getElementById('saveFile');
 
 saveFile.addEventListener('click', e => {
 	let fileName = document.getElementById('fileName').value;
 	let scene_tag = document.getElementById('scene_tag').value;
 	let npc_name = document.getElementById('npc_name').value;
+	let npcNameWith = document.getElementById('npcNameWith');
 	var text = document.getElementById('text').value;
+	var textWith = document.getElementById('textWith');
 	var buttons = ``;
 	let button1 = ``;
 	let name1 = document.getElementById('name1').value;
+	let name1With = document.getElementById('name1With');
 	let commands1 = document.getElementById('commands1').value;
 	let button2 = ``;
 	let name2 = document.getElementById('name2').value;
+	let name2With = document.getElementById('name2With');
 	let commands2 = document.getElementById('commands2').value;
 	let button3 = ``;
 	let name3 = document.getElementById('name3').value;
+	let name3With = document.getElementById('name3With');
 	let commands3 = document.getElementById('commands3').value;
 	let button4 = ``;
 	let name4 = document.getElementById('name4').value;
+	let name4With = document.getElementById('name4With');
 	let commands4 = document.getElementById('commands4').value;
 	let button5 = ``;
 	let name5 = document.getElementById('name5').value;
+	let name5With = document.getElementById('name5With');
 	let commands5 = document.getElementById('commands5').value;
 	let button6 = ``;
 	let name6 = document.getElementById('name6').value;
+	let name6With = document.getElementById('name6With');
 	let commands6 = document.getElementById('commands6').value;
 	var on_open_commands = document.getElementById('on_open_commands').value;
 	var on_close_commands = document.getElementById('on_close_commands').value;
 
-	npc_name = checkName(npc_name);
+	npc_name = checkName(npc_name, npcNameWith);
+	text = checkName(text, textWith).replaceAll("\n", "\\n");
 
-	if(!text)
-		text = ``;
-	else {
-		text = text.replaceAll("\n", "\\n")
-		text = text.replaceAll("\"", "\\\"");
-	}
+	// if(!text)
+	// 	text = ``;
+	// else {
+	// 	text = text.replaceAll("\n", "\\n")
+	// 	text = text.replaceAll("\"", "\\\"");
+	// }
 
-	button1 = checkButton(button1, name1, commands1);
-	button2 = checkButton(button2, name2, commands2);
-	button3 = checkButton(button3, name3, commands3);
-	button4 = checkButton(button4, name4, commands4);
-	button5 = checkButton(button5, name5, commands5);
-	button6 = checkButton(button6, name6, commands6);
+	button1 = checkButton(button1, name1, name1With, commands1);
+	button2 = checkButton(button2, name2, name2With, commands2);
+	button3 = checkButton(button3, name3, name3With, commands3);
+	button4 = checkButton(button4, name4, name4With, commands4);
+	button5 = checkButton(button5, name5, name5With, commands5);
+	button6 = checkButton(button6, name6, name6With, commands6);
 
 	buttons += groupButton(button1);
 	buttons += groupButton(button2);
@@ -72,8 +96,20 @@ saveFile.addEventListener('click', e => {
 		"scenes": [
 			{
 				"scene_tag": "${scene_tag}",
-				"npc_name": "${npc_name}",
-				"text": "${text}",
+				"npc_name": {
+					"rawtext": [
+						{
+							${npc_name}
+						}
+					]
+				},
+				"text": {
+					"rawtext": [
+						{
+							${text}
+						}
+					]
+				},
 				"buttons": [${buttons}\n\t\t\t\t],
 				"on_open_commands": [${on_open_commands}],
 				"on_close_commands": [${on_close_commands}]
@@ -87,13 +123,19 @@ saveFile.addEventListener('click', e => {
 		changeText("Điền thiếu");
 });
 
-function checkButton(buttonValue, nameValue, commandsValue) {
+function checkButton(buttonValue, nameValue, nameWithValue, commandsValue) {
 	if(nameValue || commandsValue) {
-		nameValue = checkName(nameValue);
+		nameValue = checkName(nameValue, nameWithValue);
 		commandsValue = checkCommands(commandsValue);
 		buttonValue = 
 		`\n\t\t\t\t\t{
-			\t\t\t"name": "${nameValue}",
+			\t\t\t"name": {
+				\t\t\t"rawtext": [
+					\t\t\t{
+						\t\t\t${nameValue}
+					\t\t\t}
+				\t\t\t]
+			\t\t\t},
 			\t\t\t"commands": [${commandsValue}]
 		\t\t\t}`;
 		return buttonValue;
@@ -102,11 +144,25 @@ function checkButton(buttonValue, nameValue, commandsValue) {
 		return ``;
 }
 
-function checkName(checkValue) {
+function checkName(checkValue, checkWith) {
 	if(!checkValue)
-		checkValue = ``;
+		checkValue = `"text": ""`;
+	else if(checkWith.style.display === 'block') {
+		if(checkWith.value) {
+			var withValue;
+			if(checkWith.value.indexOf("{") === 0)
+				withValue = `${checkWith.value}`;
+			else {
+				withValue = checkWith.value.replaceAll("\"", "\\\"");
+				withValue = `["${withValue.replaceAll(",", "\", \"")}"]`;
+			}
+			checkValue = `"translate": "${checkValue.replaceAll("\"", "\\\"")}", "with": ${withValue}`;
+		}
+		else
+			checkValue = `"translate": "${checkValue.replaceAll("\"", "\\\"")}"`;
+	}
 	else
-		checkValue = checkValue.replaceAll("\"", "\\\"");
+		checkValue = `"text": "${checkValue.replaceAll("\"", "\\\"")}"`;
 	return checkValue;
 }
 
