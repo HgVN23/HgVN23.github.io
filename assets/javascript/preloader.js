024974d07055f9eb4eb0775e85f532177e9dd0dc
Hg <83497023+HgVN23@users.noreply.github.com>
2022-08-04 13:49:58 +0700

    Update Preloader

--- assets/javascript/preloader.js
+++ assets/javascript/preloader.js
@@ -8,7 +8,18 @@ const preloader = `
 			<span class="side2" style="--i: 3"></span>
 		</div>
 	</div>
-	<div class="loading">Loading...</div>
+	<div class="loading">
+		<span style="--i: 0">L</span>
+		<span style="--i: 1">o</span>
+		<span style="--i: 2">a</span>
+		<span style="--i: 3">d</span>
+		<span style="--i: 4">i</span>
+		<span style="--i: 5">n</span>
+		<span style="--i: 6">g</span>
+		<span style="--i: 7">.</span>
+		<span style="--i: 8">.</span>
+		<span style="--i: 9">.</span>
+	</div>
 `;
 
 const preloaderCreate = document.createElement('div');
