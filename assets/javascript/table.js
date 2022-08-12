4c95ff8e6b733799b0cd1fe1065dce93daf4aa28
Hg <83497023+HgVN23@users.noreply.github.com>
2022-08-12 10:59:28 +0700

    Update table.js

--- assets/javascript/table.js
+++ assets/javascript/table.js
@@ -1,13 +1,25 @@
+const tSearch = `
+	<div class="tSearchBar">
+		<div class="searchIcon"></div>
+		<input class="tSearchInput" placeholder="Nhập để lọc theo Tên gọi" type="text">
+	</div>
+`;
+
+const tParent = document.querySelector(".table").parentElement;
+
+const tSearchCreate = document.createElement('div');
+tSearchCreate.classList.add('tSearch');
+tSearchCreate.innerHTML = tSearch;
+tParent.insertBefore(tSearchCreate, tParent.children[1]);
+
 const tSearchInput = document.querySelector(".tSearchInput");
 const rows = document.querySelectorAll("tbody tr");
 
 tSearchInput.onkeyup = (e) => {
-	const q = event.target.value.toLowerCase();
+	const tSearchValue = event.target.value.toLowerCase();
 	rows.forEach((row) => {
-		// row.querySelector("td").textContent.toLowerCase().startsWith(q) 
-		// ? (row.style.display = "table-row")
-		// : (row.style.display = "none");
-		row.querySelector("td").textContent.toLowerCase().includes(q) 
+		// row.querySelector("td").textContent.toLowerCase().startsWith(tSearchValue) 
+		row.querySelector("td").textContent.toLowerCase().includes(tSearchValue)
 		? (row.style.display = "table-row")
 		: (row.style.display = "none");
 	});
